31fe2a95d48c2c9e9d20016b85fdebafb197ba30
TEGUHMUHAMMADN <165901786+TEGUHMUHAMMADN@users.noreply.github.com>
2024-10-20 21:12:52 +0700

    Add files via upload

--- Tokserba.java
+++ Tokserba.java
@@ -11,45 +11,36 @@ package pertemuan.pkg8;
 import java.util.Scanner;
 
 public class Tokserba {
-        public static void main(String[] args) {
+       public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] kodeBarang = {"001", "002", "003", "004", "005"};
+        String[] kodeBarang = new String[5];
         String[] namaBarang = {"Penghapus", "Buku", "Pensil", "Pulpen", "Spidol"};
         int[] hargaBarang = {2000, 3000, 4000, 5000, 6000};
-        String[] inputNamaBarang = new String[5];  // For inputting item names
         int[] jumlahBeli = new int[5];
         int[] jumlahBayar = new int[5];
         int totalBayar = 0;
         for (int i = 0; i < 5; i++) {
             System.out.println("Data ke-" + (i + 1));
-            System.out.print("Masukkan Nama Barang yang ingin dibeli(Penghapus, Buku, Pensil, Pulpen, Spidol): ");
-            inputNamaBarang[i] = scanner.next();
-            System.out.print("Masukkan Jumlah barang yang ingin Beli: ");
+            System.out.print("Masukkan Kode Barang (001-005): ");
+            kodeBarang[i] = scanner.next();
+            System.out.print("Masukkan Jumlah Beli: ");
             jumlahBeli[i] = scanner.nextInt();
-            int index = -1;
-            for (int j = 0; j < namaBarang.length; j++) {
-                if (namaBarang[j].equalsIgnoreCase(inputNamaBarang[i])) {
-                    index = j;
-                    break;
-                }
-            }
-            if (index != -1) {
-                jumlahBayar[i] = hargaBarang[index] * jumlahBeli[i];
-                totalBayar += jumlahBayar[i];
-            } else {
-                System.out.println("Nama barang tidak valid!");
-                i--; 
-            }
+            int index = Integer.parseInt(kodeBarang[i]) - 1;
+            jumlahBayar[i] = hargaBarang[index] * jumlahBeli[i];
+            totalBayar += jumlahBayar[i];
         }
         for (int i = 0; i < 5; i++) {
             for (int j = i + 1; j < 5; j++) {
-                int indexI = findIndex(namaBarang, inputNamaBarang[i]);
-                int indexJ = findIndex(namaBarang, inputNamaBarang[j]);
-                
-                if (hargaBarang[indexI] > hargaBarang[indexJ]) {
-                    String tempNama = inputNamaBarang[i];
-                    inputNamaBarang[i] = inputNamaBarang[j];
-                    inputNamaBarang[j] = tempNama;
+                if (hargaBarang[i] > hargaBarang[j]) {
+                    int tempHarga = hargaBarang[i];
+                    hargaBarang[i] = hargaBarang[j];
+                    hargaBarang[j] = tempHarga;
+                    String tempNama = namaBarang[i];
+                    namaBarang[i] = namaBarang[j];
+                    namaBarang[j] = tempNama;
+                    String tempKode = kodeBarang[i];
+                    kodeBarang[i] = kodeBarang[j];
+                    kodeBarang[j] = tempKode;
                     int tempJumlahBeli = jumlahBeli[i];
                     jumlahBeli[i] = jumlahBeli[j];
                     jumlahBeli[j] = tempJumlahBeli;
@@ -63,23 +54,13 @@ public class Tokserba {
         System.out.println("************************");
         System.out.println("No\tKode Barang\tNama Barang\tHarga\tJumlah Beli\tJumlah Bayar");
         System.out.println("==============================================================");
-
         for (int i = 0; i < 5; i++) {
-            int index = findIndex(namaBarang, inputNamaBarang[i]);
-            System.out.println((i + 1) + "\t" + kodeBarang[index] + "\t\t" + namaBarang[index] + "\t\t" + hargaBarang[index] + "\t" + jumlahBeli[i] + "\t\t" + jumlahBayar[i]);
+            int index = Integer.parseInt(kodeBarang[i]) - 1;
+            System.out.println((i + 1) + "\t" + kodeBarang[i] + "\t\t" + namaBarang[index] + "\t\t" + hargaBarang[i] + "\t" + jumlahBeli[i] + "\t\t" + jumlahBayar[i]);
         }
-
         System.out.println("==============================================================");
         System.out.println("Total Bayar: " + totalBayar);
 
         scanner.close();
-    }
-    public static int findIndex(String[] namaBarang, String nama) {
-        for (int i = 0; i < namaBarang.length; i++) {
-            if (namaBarang[i].equalsIgnoreCase(nama)) {
-                return i;
-            }
-        }
-        return -1; 
     }
 }
